1a5b7a73659d19e45b26fe28f6d0b54811392523
jhonjimenez <jhonsnow73@gmail.com>
2021-06-28 21:52:54 -0500

    structure proyect folders and integration with di

---
+++ app/src/main/java/com/jhonjimenez/mercadolibretest/presentation/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.jhonjimenez.mercadolibretest.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.jhonjimenez.mercadolibretest.R
+import com.jhonjimenez.mercadolibretest.databinding.ActivityMainBinding
+import dagger.android.AndroidInjection
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val host =
+            supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment?
+                ?: return
+        navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.mainFragment),
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+}
