3fc4ee229df27a50c184bed109aa978e0c825854
jhonjimenez <jhonsnow73@gmail.com>
2021-06-28 19:07:37 -0500

    first commit

---
+++ app/src/main/java/com/jhonjimenez/mercadolibretest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jhonjimenez.mercadolibretest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
